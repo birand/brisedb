71b9d52385330c5d3a934e5ea37f0828c1718c05
Birand Önen <birandbosley@gmail.com>
2021-05-06 00:03:13 +0300

    Commit()

--- brise.go
+++ brise.go
@@ -51,5 +51,25 @@ func (ts *TransactionStack) Peek() *Transaction {
 	return ts.top
 }
 
+/* Commit write(SET) changes to the store with TransactionStack scope
+Also write cahnges to disk/file,
+if data needs to persist after the shell closes
+*/
+func (ts *TransactionStack) Commit() {
+	ActiveTransaction := ts.Peek()
+	if ActiveTransaction != nil {
+		for key, value := range ActiveTransaction.store {
+			GlobalStore[key] = value
+			if ActiveTransaction.next != nil {
+				// update the parent transaction
+				ActiveTransaction.next.store[key] = value
+			}
+		}
+	} else {
+		fmt.Printf("INFO: Nothing ot commit\n")
+	}
+	// TODO: write data to file to make it persist to disk
+	// Tip: serialize map data to JSON
+}
 func main() {
 }
